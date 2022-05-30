a52ed7d8353724b852285c992549fd6d5adb9493
Abdlwahid <natabd005@gmail.com>
2022-05-30 22:37:09 +0300

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks to see if a list is in an endless loop or cycle
+ * @list: the list to check
+ * Return: 0 if no cycle is detected, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
