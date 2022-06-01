598eb7077983ef73c304c0daa7795cd74afd663a
Abdlwahid <natabd005@gmail.com>
2022-06-01 11:26:34 +0300

    Challenged

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,43 +1,32 @@
 #include "lists.h"
-
 /**
- * check_cycle - checks if a singly linked list has
- * a cycle in it
- * @list: pointer to the list
- * Return: 0 if there is no cycle,
- * 1 if there is a cycle
+ * check_cycle - cycle tortoise and hare
+ * @list: pointer to head
+ * Return: 1 on success, 0 otherwise.
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *p2;
-	listint_t *prev;
+	listint_t *tortoise;
+	listint_t *hare;
 
-	p2 = list;
-	prev = list;
-	while (list && p2 && p2->next)
+	if (list == NULL)
+		return (0);
+	tortoise = list;
+	hare = list;
+	while (hare->next != NULL && hare->next->next != NULL)
 	{
-		list = list->next;
-		p2 = p2->next->next;
-
-		if (list == p2)
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
 		{
-			list = prev;
-			prev =  p2;
-			while (1)
+			tortoise = list;
+			while (tortoise != hare)
 			{
-				p2 = prev;
-				while (p2->next != list && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == list)
-					break;
-
-				list = list->next;
+				tortoise = tortoise->next;
+				hare = hare->next;
 			}
 			return (1);
 		}
 	}
-
 	return (0);
 }
