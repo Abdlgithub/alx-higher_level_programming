56de67f06a7cec2e9a3f26b6596e83fdd1637c10
Abdlwahid <natabd005@gmail.com>
2022-06-29 00:51:35 +0300

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" say_my_name module """
+
+
+def say_my_name(prmFirstName, prmLastName=""):
+    """ say_my_name function
+
+    this function concatenated first name and last name and print the result
+
+    Attributes:
+        prmFirstName: first name
+        prmLastName: last name
+    """
+    if not isinstance(prmFirstName, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(prmLastName, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(prmFirstName, prmLastName))
