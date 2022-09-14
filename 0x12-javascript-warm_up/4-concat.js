052f3a981797be96ac5b46caf4fbabc6dce2029a
Abdlwahid <natabd005@gmail.com>
2022-09-14 11:22:46 +0300

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const argv1 = argv[2]; const argv2 = argv[3];
-console.log(argv1 + ' is ' + argv2);
+console.log(process.argv[2] + ' is ' + process.argv[3])
